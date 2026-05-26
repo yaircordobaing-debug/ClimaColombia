061e72860030b5e2393763eaf5101bbbeae153c5
AndresLegro <andreslegrog@gmail.com>
2026-05-26 11:53:35 -0500

    feat: Adding docker files and docker compose to implement CI/CD

--- FrontEnd/src/config/weatherAPI.ts
+++ FrontEnd/src/config/weatherAPI.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://climacolombia.onrender.com/api/weather";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "/api/weather";
 
 export const ENDPOINTS = {
   currentWeather: "/current",
